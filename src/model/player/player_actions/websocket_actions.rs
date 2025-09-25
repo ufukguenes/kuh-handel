3e7562d6eeb7d45890f04fd5a220f0e88f97fafc
ufukguenes <u.guenes@live.de>
2025-09-25 20:18:54 +0200

    continued websocket handeling

--- src/model/player/player_actions/websocket_actions.rs
+++ src/model/player/player_actions/websocket_actions.rs
@@ -13,8 +13,29 @@ use axum::{
 use tokio::sync::mpsc;
 
 pub struct WebsocketActions {
+    // for each bot, create two channels
+    // 1. to send messages containing the actions received from the client-bot over the websocket to the server-bot from our logic (from action_sender to action_receiver)
+    // 2. to send the game state and possible actions that that are called from the server on the bot (from state_sender to state_receiver)
+    // to the websocket so it can send it to the client-bot
+
+    // used by the player
     state_sender: mpsc::Sender<Message>,
     action_receiver: mpsc::Receiver<Message>,
+
+    // used by the WebSocket connection
+    state_receiver: mpsc::Receiver<Message>,
+    action_sender: mpsc::Sender<Message>,
+}
+
+impl WebsocketActions {
+    pub fn get_channels(&self) -> (&mpsc::Receiver<Message>, &mpsc::Sender<Message>) {
+        (&self.state_receiver, &self.action_sender)
+    }
+
+    pub fn close_connections(&mut self) {
+        self.state_receiver.close();
+        self.action_receiver.close();
+    }
 }
 
 impl PlayerActions for WebsocketActions {

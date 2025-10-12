d607532ae116c214a54922054c33b0f09d48bb66
ufukguenes <u.guenes@live.de>
2025-10-12 21:49:41 +0200

    fixing deadlock and missing players

--- src/backend_api.rs
+++ src/backend_api.rs
@@ -3,13 +3,14 @@ use crate::model::game_errors::GameError;
 use axum::{
     extract::{
         Query, State,
+        connect_info::Connected,
         ws::{Message, WebSocket, WebSocketUpgrade},
     },
     response::IntoResponse,
 };
 pub use axum_macros::debug_handler;
-use std::collections::BTreeMap;
 use std::sync::Arc;
+use std::{collections::BTreeMap, os::linux::raw::stat};
 use tokio::sync::Mutex;
 use tokio::sync::mpsc::{Receiver, Sender};
 use tracing::{error, info};
@@ -50,12 +51,14 @@ impl WebsocketGame {
     }
 
     pub async fn get_missing_players(&self) -> Vec<String> {
-        let mut missing_players = Vec::new();
         let locked_connected_player = self.connected_players.lock().await;
-        for player_id in self.channel_per_player.lock().await.keys() {
-            let is_connected = locked_connected_player.get(player_id).unwrap();
+        let player_ids: Vec<String> = locked_connected_player.keys().cloned().collect();
+
+        let mut missing_players = Vec::new();
+        for current_id in player_ids {
+            let is_connected = locked_connected_player.get(&current_id).unwrap();
             if !is_connected {
-                missing_players.push(player_id.clone());
+                missing_players.push(current_id.clone());
             }
         }
         missing_players
@@ -80,20 +83,20 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<Mutex<WebsocketGame>>, 
     // 2. to send the game state and possible actions that that are called from the server on the bot (from state_sender to state_receiver)
     // to the websocket so it can send it to the client-bot
 
-    info!("bck | Bot connected with ID: {}", player_id.clone());
-    let (mut state_receiver, action_sender) = state
-        .lock()
-        .await
-        .channel_per_player
-        .lock()
-        .await
-        .remove(&player_id)
-        .unwrap();
+    let channels = {
+        let state_lock = state.lock().await;
+        Arc::clone(&state_lock.channel_per_player)
+    };
 
-    state
-        .lock()
-        .await
-        .connected_players
+    info!("bck | Bot connected with ID: {}", player_id.clone());
+    let (mut state_receiver, action_sender) = channels.lock().await.remove(&player_id).unwrap();
+
+    let connected_players = {
+        let state_lock = state.lock().await;
+        Arc::clone(&state_lock.connected_players)
+    };
+
+    connected_players
         .lock()
         .await
         .insert(player_id.clone(), true);
@@ -107,72 +110,83 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<Mutex<WebsocketGame>>, 
         );
         let recv_state: Option<Message> = state_receiver.recv().await;
 
-        match recv_state {
+        let state_msg = match recv_state {
             Some(msg) => {
-                info!("bck | received game state info for bot {}", player_id);
-
                 info!(
-                    "bck | game state info for bot {}: {}",
+                    "bck | received game state info for bot {}: {}",
                     player_id,
                     msg.to_text().unwrap()
                 );
-
-                info!("bck | waiting to send state to client of bot {}", player_id);
-
-                if let Err(e) = socket.send(msg).await {
-                    error!("bck | Error sending message: {}", e);
-                }
-                info!(
-                    "bck | finished sending state to client of bot {}",
-                    player_id
-                );
-
-                // receive action and send to server-bot
-                info!(
-                    "bck | waiting to receive action from client of bot {}",
-                    player_id
-                );
-                if let Some(Ok(msg)) = socket.recv().await {
-                    info!(
-                        "bck | bot {} action: {}",
-                        player_id,
-                        msg.clone().to_text().unwrap()
-                    );
-                    info!(
-                        "bck | finished receiving action from client of bot {}",
-                        player_id
-                    );
-
-                    info!("bck | waiting to send action of bot {} to game", player_id);
-                    match msg {
-                        Message::Text(_) => match action_sender.send(msg).await {
-                            Ok(_) => {
-                                info!("bck | action of bot {} has been send to game", player_id)
-                            }
-                            Err(_) => {
-                                error!(
-                                    "bck | failure sending action of bot {} to game, closing connection",
-                                    player_id
-                                );
-                                break;
-                            }
-                        },
-                        Message::Close(_) => {
-                            info!("bck | Closing WebSocket connection of bot {}.", player_id);
-                            break;
-                        }
-                        _ => {
-                            error!(
-                                "bck | received unknown message type from client of bot {}, closing WebSocket connection",
-                                player_id
-                            );
-                            break;
-                        }
-                    }
-                }
+                msg
             }
             None => {
-                info!("bck | game closed connection to bot {}.", player_id);
+                info!(
+                    "bck | game closed connection to bot {}, ending loop",
+                    player_id
+                );
+                break;
+            }
+        };
+
+        info!("bck | waiting to send state to client of bot {}", player_id);
+        if let Err(e) = socket.send(state_msg).await {
+            error!("bck | Error sending message: {}", e);
+            break;
+        };
+
+        info!(
+            "bck | finished sending state to client of bot {}",
+            player_id
+        );
+
+        let action_msg = socket.recv().await;
+
+        let action_msg = match action_msg {
+            Some(Ok(msg)) => {
+                info!(
+                    "bck | bot {} action: {}",
+                    player_id,
+                    msg.clone().to_text().unwrap()
+                );
+                info!(
+                    "bck | finished receiving action from client of bot {}",
+                    player_id
+                );
+                msg
+            }
+            Some(Err(e)) => {
+                error!("bck | error receiving from bot {}: {}", player_id, e);
+                break;
+            }
+            None => {
+                info!("bck | bot {} disconnected.", player_id);
+                break;
+            }
+        };
+
+        match action_msg {
+            Message::Text(_) => match action_sender.send(action_msg).await {
+                Ok(_) => {
+                    info!("bck | action of bot {} has been send to game", player_id)
+                }
+                Err(_) => {
+                    error!(
+                        "bck | failure sending action of bot {} to game, closing connection",
+                        player_id
+                    );
+                    break;
+                }
+            },
+
+            Message::Close(_) => {
+                info!("bck | Closing WebSocket connection of bot {}.", player_id);
+                break;
+            }
+            _ => {
+                error!(
+                    "bck | received unknown message type from client of bot {}, closing WebSocket connection",
+                    player_id
+                );
                 break;
             }
         }
@@ -180,13 +194,7 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<Mutex<WebsocketGame>>, 
 
     info!("bck | Bot ID {} disconnected.", player_id);
 
-    state
-        .lock()
-        .await
-        .connected_players
-        .lock()
-        .await
-        .insert(player_id, false);
+    connected_players.lock().await.insert(player_id, false);
 
     state_receiver.close();
 }
@@ -194,22 +202,22 @@ async fn handle_socket(mut socket: WebSocket, state: Arc<Mutex<WebsocketGame>>, 
 pub async fn organize_new_game(state: Arc<Mutex<WebsocketGame>>) {
     let mut missing_players = state.lock().await.get_missing_players().await;
     while missing_players.len() > 0 {
-        info!("bck | Waiting for missing players: {:?}", missing_players);
+        info!("og | Waiting for missing players: {:?}", missing_players);
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
         missing_players = state.lock().await.get_missing_players().await;
     }
 
-    info!("bck | all players joined");
+    info!("og | all players joined");
     loop {
         missing_players = state.lock().await.get_missing_players().await; // todo this does not work correctly
         if missing_players.len() > 0 {
             info!(
-                "The game should be interrupted and the following players removed: {:?}",
+                "og | The game should be interrupted and the following players removed: {:?}",
                 missing_players
             );
         }
 
-        info!("bck | It's bot ID ___'s turn.",);
+        info!("og | It's bot ID ___'s turn.",);
         // todo, should i use this: state.game.play_one_round();
 
         // todo: do i want to keep this (either the sleep or in general the organize game)

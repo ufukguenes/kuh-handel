a0e273da7aa4491c39e987223cccaee54a010259
ufukguenes <u.guenes@live.de>
2025-12-20 12:25:00 +0100

    stubs

--- pyhandel/pyhandel/player/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import base_player
-from . import random_player
-from . import simple_player
-from . import wallet
-from . import player_actions
---
+++ pyhandel/pyhandel/player/__init__.pyi
@@ -0,0 +1,5 @@
+from . import base_player as base_player
+from . import random_player as random_player
+from . import simple_player as simple_player
+from . import wallet as wallet
+from . import player_actions as player_actions

7f2d37df6fc3b50ce521abfded1c0cd77a2dfedf
ufukguenes <u.guenes@live.de>
2025-12-20 12:58:47 +0100

    update stubs

--- pyhandel/pyhandel/__init__.pyi
+++ pyhandel/pyhandel/__init__.pyi
@@ -1,4 +1,6 @@
-from . import player as player
+from .player import player_actions as player_actions
+from .player import simple_player as simple_player
+
 from . import messages as messages
 from . import animals as animals
 from . import client as client

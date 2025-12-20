55192b0383d6ad103a67a622f0eee46e4f54791a
ufukguenes <u.guenes@live.de>
2025-12-20 12:47:05 +0100

    update stubs

--- pyhandel/pyhandel/__init__.pyi
+++ pyhandel/pyhandel/__init__.pyi
@@ -1,7 +1,16 @@
 from . import player as player
 from . import messages as messages
+from . import animals as animals
+from . import client as client
+
+from typing import TypeAlias
+
+Money: TypeAlias = int
+Value: TypeAlias = int
 
 __all__ = [
     "player",
     "messages",
+    "animals",
+    "client",
 ]

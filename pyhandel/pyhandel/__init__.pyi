6846f24aa4d00e6f89b4ffff6f9d4d2186fff694
ufukguenes <u.guenes@live.de>
2025-12-20 12:17:16 +0100

    add __init__ stub file

---
+++ pyhandel/pyhandel/__init__.pyi
@@ -0,0 +1,14 @@
+from .player import player_actions as player_actions
+from .player import simple_player as simple_player
+from .player import random_player as random_player
+from .player import wallet as wallet
+from .player import base_player as base_player
+from .messages import *
+
+__all__ = [
+    "player_actions",
+    "simple_player",
+    "random_player",
+    "wallet",
+    "base_player",
+]
